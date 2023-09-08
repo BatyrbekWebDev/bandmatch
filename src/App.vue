61682edb6967fd463efb1a6f50927891a1259ed6
Batyrbek <43727077+BatyrbekWebDev@users.noreply.github.com>
2023-09-08 15:42:05 +0300

    update

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,63 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="main-layout">
+    <div class="main-layout__wrap">
+      <heading text="Find your music match" counter="1"></heading>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Heading from './components/Heading.vue';
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    Heading,
+  },
+};
 </script>
-
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+@import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+
+html {
+  font-size: 0.625em;
+  box-sizing: border-box;
+  text-size-adjust: 100%;
+  text-rendering: optimizeSpeed;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: inherit;
+}
+
+body {
+  font-family: 'Roboto', sans-serif;
+  font-size: 1.6rem;
+  color: rgba(0, 0, 0, 0.87);
+  line-height: normal;
+  background: #fafafa;
+  min-height: 100vh;
+  overflow-x: hidden;
+  margin: 0;
+}
+
+.main-layout {
+  min-height: 100vh;
+  padding-top: 100px;
+  padding-bottom: 50px;
+
+  &__wrap {
+    max-width: 74rem;
+    padding: 0 20px;
+    margin: 0 auto;
+  }
+}
+
+@media only screen and (max-width: 600px) {
+  .main-layout {
+    padding-top: 50px;
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,3 +2,4 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+// 96947fcd4691d63e810b11534de4c16d
